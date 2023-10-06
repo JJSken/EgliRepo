f89a6c5110ae0f904a5df571f88c05ace9b119bf
JJSken <32110435@student.uwl.ac.uk>
2023-10-06 12:51:14 +0100

    2nd

---
+++ test.jsx
@@ -0,0 +1,9 @@
+const String = "Hello World";
+//export here if u want to ecport onle this func and not the whole jsx right?
+export const test = () => {
+    knp
+    return ({
+        knp
+    })
+}
+export default test;
